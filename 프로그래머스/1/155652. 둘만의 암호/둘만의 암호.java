bbfd38020389a0a1802b2d68af1a2f168e712b20
ossongh <63629844+ohsonghyun@users.noreply.github.com>
2024-02-08 16:53:33 +0900

    [level 1] Title: 둘만의 암호, Time: 1.94 ms, Memory: 80.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+class Solution {
+    public String solution(String s, String skip, int index) {
+        StringBuffer sb = new StringBuffer();
+        
+        for(char x : s.toCharArray()) {
+            for (int i = 0; i < index; i++) {
+                x++;
+                x -= x > 'z' ? 26 : 0;
+                i -= skip.contains(String.valueOf(x)) ? 1 : 0;
+            }
+            sb.append(x);
+        }
+        
+        return sb.toString();
+    }
+}
