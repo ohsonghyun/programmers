53ed36cda32d37f6f8d63647ad7b28c7917621f1
ossongh <63629844+ohsonghyun@users.noreply.github.com>
2024-02-28 23:41:23 +0900

    [level 2] Title: [1차] 캐시, Time: 1.76 ms, Memory: 73.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int cacheSize, String[] cities) {
+        if(cacheSize == 0){ 
+            return cities.length * 5;
+        }
+        
+        int answer = 0;
+        List<String> cache = new ArrayList<>();
+        
+        for(int i = 0; i < cities.length; i++){
+            String city = cities[i].toUpperCase(); 
+
+            if(cache.contains(city)){ 
+                cache.remove(city); 
+                cache.add(city);
+                answer++;
+            } else{
+                if(cache.size() == cacheSize){ 
+                    cache.remove(0); 
+                }
+                cache.add(city);
+                answer += 5;
+            }
+        }
+
+        return answer;
+    }
+}
